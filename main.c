319ee4641aa3b498ef9ca7ee1068a2aa8dcd6372
Michael Maurice <michaelmaurice92@gmail.com>
2019-04-03 10:53:22 +0000

    Finished code

--- main.c
+++ main.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+
+int main() {
+       
+// Maximum integer to test. Declared constant
+// This is declared so that the maximum integer
+// can easily be changed
+    const int MAX = 100;
+    char numbers[MAX+1];
+    int index;
+    int j;
+// Initialised numbers[]
+    for(index = 0; index <= MAX; index++) {
+    numbers[index] = 1;
+    }
+    
+    for(index = 2; index <= MAX; index++){ 
+        for(j = 2*index; j < MAX; j += index){
+        numbers[j] = 0;
+        }
+    }
+
+// DO THE SIEVE ALGORITHM HERE
+// for(index = 2; ...etc
+// Print results
+    printf("The following numbers are prime:\n");
+    for(index = 2; index < MAX; index++) {
+        if(numbers[index] == 1) {
+        printf("%d\n", index);
+        }
+    }
+    
+    
+return 0;
+}
 
